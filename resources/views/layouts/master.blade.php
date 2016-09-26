05c52884151eb346651f027228e983f1547132cd
Govinda <dahalgovinda2014@gmail.com>
2016-09-26 14:14:58 +0545

    learn Gmap js api

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-        <html lang="en">
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Laravel Map</title>
+    <title>
+        Laramap
+    </title>
+
     </meta>
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    </link>
 </head>
 <body>
 @yield('content')
@@ -14,7 +17,7 @@
 </script>
 {{-- Google map api  --}}
 
-<script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4-t9LZzchrhV5CVNKRCl0ber85k14g2E&libraries=places">
+<script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7a-pVRxc_cx00QNTiPWQZW50qxiqZGO0&libraries=places">
 </script>
 
 <script src="{{asset('js/script.js')}}"></script>
